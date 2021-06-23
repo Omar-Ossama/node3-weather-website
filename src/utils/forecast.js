478fea53b57faa39e9445fd8fb7aee1e4cc391a8
Omar Ossama <you@example.com>
2021-06-23 09:54:23 +0200

    oraganizing forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 
 const forecast = (latitude, longitude, callback) => {
-    const url ='http://api.weatherstack.com/current?access_key=f0a91c3742174f3ad7fd204a4b91e0d3&query=' + latitude + ',' + longitude + '&units=s'
+    const url ='http://api.weatherstack.com/current?access_key=f0a91c3742174f3ad7fd204a4b91e0d3&query=' + latitude + ',' + longitude + '&units=m'
 
     request({ url, json: true }, (error, {body}) => {
         if(error){
@@ -14,7 +14,7 @@ const forecast = (latitude, longitude, callback) => {
 
     } else {
 
-        callback(undefined, body.current.weather_descriptions[0]+'. the tempreture is '+ body.current.temperature +' degrees out and feels like '+ body.current.feelslike + ' degrees in' )
+        callback(undefined, body.current.weather_descriptions[0]+'. The tempreture is '+ body.current.temperature +' degrees out and feels like '+ body.current.feelslike + ' degrees in with ' + body.current.humidity+'% humidity and wind speed of ' + body.current.wind_speed + ' km/h' )
         
     }
 
